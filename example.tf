510209463f6911ed0ef8a2956487c18d9a3f9cea
od3n <mr.od3n@gmail.com>
2020-03-23 21:46:17 +0800

    Resource Dependencies

--- example.tf
+++ example.tf
@@ -7,3 +7,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0b21b3ea2cb8660a5"
   instance_type = "t2.micro"
 }
+
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
